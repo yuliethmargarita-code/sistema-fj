aa10a9299b6180469c043d5b724b935a332f4c08
yuliethmargarita-code <yulieth.margarita05@gmail.com>
2026-05-12 01:40:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+from cliente import Cliente
+from servicio import ReservaSala, AlquilerEquipo
+from reserva import Reserva
 
+try:
+
+    cliente1 = Cliente("Laura", "laura@gmail.com")
+
+    servicio1 = ReservaSala("Sala VIP", 100)
+
+    reserva1 = Reserva(cliente1, servicio1, 2)
+
+    reserva1.confirmar()
+
+    print(reserva1.mostrar_reserva())
+
+except Exception as e:
+    print("Error:", e)
+
+finally:
+    print("Programa finalizado")
